9ec76f82ee3881f8dac0f7ba24d77cf2a080703f
bettercallanshuman <bettercallanshuman@gmail.com>
2026-04-26 18:12:26 +0530

    feat : Solved 2 more questions of chapter 1

---
+++ Chapter 1/Chapter 1 PS/problem-4.py
@@ -0,0 +1,9 @@
+#  PS: Write a python program to print the contents of a directory using the os module. 
+# Search online for the function which does that.
+
+
+import os
+
+os.getcwd()
+print(os.listdir())
+    
---
+++ Chapter 1/Chapter 1 PS/problem-5.py
@@ -0,0 +1,11 @@
+# Label the program written in problem 4 with comments.  
+
+# Author: Anshuman Das
+# Date: 2026-04-26
+# PS5: Labeling the directory lister program with comments
+
+import os  # Importing the built-in 'os' module to interact with the operating system
+
+
+os.getcwd() # The getcwd() function returns the current working directory as a string.
+print(os.listdir()) # The listdir() function returns a list of the names of the entries in the directory given by path. If no path is specified, it defaults to the current working directory. Here, it will print the contents of the current directory.  
