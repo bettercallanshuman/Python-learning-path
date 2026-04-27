310c9fead4f7d5b8ea23ba4e138aca3b419d6ab8
bettercallanshuman <bettercallanshuman@gmail.com>
2026-04-27 21:58:20 +0530

    feat : complete escape sequence

---
+++ Chapter 3/05_escape-seq.py
@@ -0,0 +1,8 @@
+a = "anshuman is very sharp, \nbut he procrastinates a lot" # here \n is newline, and creates a newline from where it placed in the str.
+print(a)
+
+b = "you are the best guy \tin the room" # \t is tab, it creates a tab when the str got printed.
+print(b)
+
+c = "this is \"Udaipur\"" # \"\" this brings the particular str in between where it has been used.
+print(c)
