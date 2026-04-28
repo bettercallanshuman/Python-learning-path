8c0993288ff9cc2e9defff16d6ae7156f6314dfa
bettercallanshuman <bettercallanshuman@gmail.com>
2026-04-28 18:01:49 +0530

    feat : solve problem 1

---
+++ Chapter 4 - Lists & Tuples/Chapter 4 PS/problem-1.py
@@ -0,0 +1,21 @@
+#. Write a program to store seven fruits in a list entered by the user. 
+
+fruits = []
+
+f1 = input("Enter fruit's name 1 :")
+fruits.append(f1)
+f2 = input("Enter fruit's name 2 :")
+fruits.append(f2)
+f3 = input("Enter fruit's name 3 :")
+fruits.append(f3)
+f4 = input("Enter fruit's name 4 :")
+fruits.append(f4)
+f5 = input("Enter fruit's name 5 :")
+fruits.append(f5)
+f6 = input("Enter fruit's name 6 :")
+fruits.append(f6)
+f7 = input("Enter fruit's name 7 :")
+fruits.append(f7)
+
+print(fruits)
+print("Order Placed Successfully ! Arriving in 10 mins.")
