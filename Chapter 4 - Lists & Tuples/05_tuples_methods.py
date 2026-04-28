f1d226a7c4137de63eee020bb98222dd63fb2106
bettercallanshuman <bettercallanshuman@gmail.com>
2026-04-28 17:32:28 +0530

    feat : tuples methods done. chapter 4 finish.

---
+++ Chapter 4 - Lists & Tuples/05_tuples_methods.py
@@ -0,0 +1,15 @@
+a = (1, 69, 3, "rohan", 4.098, "lila",69)
+print(a.count(69)) #  Counts :  how many same elements that are mentioned are there in the tuple.
+
+print(a.index("lila")) # Index : finds the index of the element.
+
+b = (2, 67, 90, "Anshuman", 707070.34, "hahahahaha")
+print(a+b) # concatenation.
+
+print(a*3) # Repetation. : tuple*(no. of times this tuple wants to get repeated)
+
+print(69 in b) # Membership : ("element" in "tuple"). Always returns boolean.
+print(69 in a ) # Membership : ("element" in "tuple"). Always returns boolean.
+
+print(len(b)) # Length : length of tuple.
+    
