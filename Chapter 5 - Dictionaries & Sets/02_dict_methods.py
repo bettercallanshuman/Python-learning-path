7c4182de1dc3dca63f43280a6c5c0c21d7c0a9fd
bettercallanshuman <bettercallanshuman@gmail.com>
2026-04-29 23:44:35 +0530

    feat : complete dict methods with tricky diff betwn using dict.get(keys) and dict[keys]

--- Chapter 5 - Dictionaries & Sets/02_dict_methods.py
+++ Chapter 5 - Dictionaries & Sets/02_dict_methods.py
@@ -6,6 +6,22 @@ marks =  {
 
 #RHS part is known as "keys" and the LHS is known as "values"
 
-#print(marks.items())
+print(marks.items())
 print(marks.keys())
 print(marks.values())
+
+marks.update({"Anshuman" : 99, "Renuka" : 78})
+print(marks)
+
+### INTERVIEW QUESTION ( TRICKY )
+# Whats's the diffrence between using "()" and "[]", if these functions returns the same value.
+print(marks.get("Anshuman"))
+print(marks["Anshuman"])
+#Output : 100
+#Output : 100
+
+#Answer : if we change the keys, the .get method will return "None", whereas the other won't return it'll show "Key Error"
+print(marks.get("Anshuman1"))
+print(marks["Anshuman1"])
+
+#Check out other Dictionaries ".pop", ".pop item"
