724b1a9b551fbeda5e163a74887b13cc97e49d43
bettercallanshuman <bettercallanshuman@gmail.com>
2026-04-30 22:51:28 +0530

    progress : learnt elif conditional expression

--- Chapter 6 - Conditional Expression/01_conditional.py
+++ Chapter 6 - Conditional Expression/01_conditional.py
@@ -4,5 +4,8 @@ if(a>=18):
     print("You are above the age.") #the space before print is known as "Indentation"
     print("Good to go.")
 
+elif(a<0):
+    print("You are entering an invalid age.")
+
 else:
     print("Sorry, you are under age.")
