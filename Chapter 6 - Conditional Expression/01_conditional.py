6e37b680d2777617dd3cda2fb637c6bd4cc0f9b1
bettercallanshuman <bettercallanshuman@gmail.com>
2026-04-30 22:50:50 +0530

    progress : learnt if-else conditional expression.

---
+++ Chapter 6 - Conditional Expression/01_conditional.py
@@ -0,0 +1,8 @@
+a = int(input("Enter your age : "))
+
+if(a>=18):
+    print("You are above the age.") #the space before print is known as "Indentation"
+    print("Good to go.")
+
+else:
+    print("Sorry, you are under age.")
