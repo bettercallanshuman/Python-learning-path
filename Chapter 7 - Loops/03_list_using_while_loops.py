cd3ebcbb7cf4af0c6180e15a221c2efb946d5ab2
bettercallanshuman <bettercallanshuman@gmail.com>
2026-05-01 13:44:45 +0530

    progress : list using while loops

---
+++ Chapter 7 - Loops/03_list_using_while_loops.py
@@ -0,0 +1,19 @@
+l = ["Anshuman", 1, "tray", 78.0909, "Alex"]
+
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i += 1
+
+#This same statement in for loop.
+
+for bro in l:
+    print(bro)
+
+#  for: Tells Python, "I want to repeat an action."
+
+#  item: This is a temporary variable. You can call it anything—bro, val, x, or member.
+
+#  in: Tells Python which collection to look at.
+
+#  l: Your list.
